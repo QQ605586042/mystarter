d6956b5f8359efa30028b7fa9d4ffd9333084dce
二郎神 <605586042@qq.com>
2020-01-12 21:07:55 +0800

    spring 自定义starter

---
+++ mystartertest/src/test/java/com/younger/AppTest.java
@@ -0,0 +1,21 @@
+package com.younger;
+
+import com.younger.controller.App;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+
+@SpringBootTest(classes = App.class)
+@RunWith(SpringRunner.class)
+public class AppTest {
+    @Autowired
+    private Jedis jedis;
+    @Test
+    public void testJedis(){
+        jedis.del("name");
+    }
+
+}
---
+++ redisstarter/src/main/java/com/younger/redis/RedisAutoConfiguration.java
@@ -0,0 +1,27 @@
+package com.younger.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
+
+@Configuration
+@ConditionalOnClass(Jedis.class)
+@EnableConfigurationProperties(RedisProperties.class)
+@ConditionalOnProperty(prefix = "redis", value = "enabled", matchIfMissing = true)
+public class RedisAutoConfiguration {
+    private final Logger logger = LoggerFactory.getLogger(RedisAutoConfiguration.class);
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Jedis jedis(RedisProperties redisProperties) {
+        logger.info("-------------------->要初始化jedis啦");
+        Jedis jedis = new Jedis(redisProperties.getHost(), redisProperties.getPort());
+        return jedis;
+    }
+}
